d0fe881ed9a4eea66fdcd8e3bc48ee282ea38a5e
lukaskup <lukasz.skup1999@gmail.com>
2022-06-30 16:53:33 +0200

    firebase register ready, map fragment init

--- app/src/main/java/com/example/eventmanager/fragments/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/eventmanager/fragments/auth/RegisterFragment.kt
@@ -9,28 +9,16 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.eventmanager.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
@@ -45,29 +33,22 @@ class RegisterFragment : Fragment() {
         }
 
         view.findViewById<Button>(R.id.registerButton).setOnClickListener{
+            val email = view.findViewById<TextView>(R.id.textRegisterEmail).text.toString();
+            val password = view.findViewById<TextView>(R.id.textRegisterPassword).text.toString();
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener() {task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    findNavController().navigate()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    System.out.println(task.exception)
+                }
+            }
+
 
         }
 
         return view
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
